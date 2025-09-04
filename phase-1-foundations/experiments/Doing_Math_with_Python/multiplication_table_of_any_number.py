da69f254f163545ac62f9e9f9d0909fd0bc6361a
nicklife <nicklife@localhost.localdomain>
2025-09-04 10:37:12 +0300

    updated multiplication table calculator

--- phase-1-foundations/experiments/Doing_Math_with_Python/multiplication_table_of_any_number.py
+++ phase-1-foundations/experiments/Doing_Math_with_Python/multiplication_table_of_any_number.py
@@ -1,20 +1,35 @@
 '''
-Multiplication table of any number 1 - 15
+Multiplication table of any number
 '''
 def math_table():
+    """
+    Prompts the user to enter a number and a multiple, then prints the multiplication table
+    for that number up to the specified multiple. Continues until the user chooses to exit.
+    """
+    while True:
+        try:
+            # Ask user for the base number (can be float)
+            x = float(input("Please enter a number to get multiplication table : "))
+            # Ask user for the number of multiples (must be integer)
+            m = int(input("which multiple : "))
+            # Validate inputs
+            if x == 0.0 or m == 0:
+                print("Enter a valid number")
+            else:
+                # Print multiplication table up to m
+                for i in range(1, m + 1):
+                    result = x * i
+                    print(f"{x} x {i} = {round(result, 2)}")
+                # Ask if user wants to continue
+                cont = input("Do you want to continue (y/n): ")
+                if cont.lower() != "y":
+                    break
+        except ValueError:
+            # Handle invalid input
+            print("Enter a valid number!")
 
-    x = input("Please enter a number to get multiplication table 1 - 15: ")
-    try:
-        x = float(x)
-        if x > 0 and x.is_integer or float:
-            for i in range(1, 16):
-                result = x * i
-                print(f"{x} x {i} = {round(result, 2)}")
-    except ValueError:
-        print("Please input a valid number!")
-    
-
-if  __name__ == "__main__":
+if __name__ == "__main__":
+    # Run the multiplication table function if script is executed directly
     math_table()
 
 
