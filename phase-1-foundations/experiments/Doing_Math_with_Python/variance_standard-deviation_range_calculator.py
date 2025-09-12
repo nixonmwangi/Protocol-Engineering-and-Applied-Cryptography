5cb5ba5f74780b610e37a1e6a10a8f00484b0522
nicklife <nicklife@localhost.localdomain>
2025-09-12 23:51:11 +0300

    Range, Variance and Standard Deviation python script

---
+++ phase-1-foundations/experiments/Doing_Math_with_Python/variance_standard-deviation_range_calculator.py
@@ -0,0 +1,82 @@
+#calculate the Range, Variance and Standard Deviation
+def range(values):
+    lowest = min(values)
+    highest = max(values)
+
+    delta = highest - lowest
+
+    return delta, highest, lowest
+
+def variation(values):
+    #Mean
+
+    l = len(values)
+    s = sum(values)
+    mean = (s/l)
+
+    print(f"mean: {mean}")
+
+    #Differences
+    diff = []
+
+    for v in values:
+        d = mean - v
+        diff.append(d)
+
+    #Variance
+
+    squared_diff = []
+
+    for d in diff:
+        squared_diff.append(d**2)
+
+    variance = float(sum(squared_diff)/len(values))
+   
+    #Standard variance
+
+    std = variance**0.5
+
+    return variance, std
+
+        
+        
+    
+
+if __name__ == "__main__":
+    print("Calculate the Range, Variance, Standard deviation and  Coefficient correlation ")
+    print("Press Enter or 'q' to finish input")
+
+    values = []
+
+    while True:
+        
+        v = input("Enter value : ")
+
+        if v == "" or v.lower() == "q":
+            print("Done")
+            break
+
+        try:
+            value = int(v)
+            values.append(value)
+
+        except ValueError:
+            try:
+                value = float(v)
+                values.append(value)
+
+            except:
+                print("Invalid input, please enter a number")
+                continue
+
+    delta, highest, lowest = range(values)
+    variance, std = variation(values)
+
+    print(f"The Range is {delta} with highest value {highest} and lowest values {lowest}")
+    print(f"Variance           : {variance:.2f}")
+    print(f"Standard deviation : {std:.2f}")
+
+            
+        
+
+        
